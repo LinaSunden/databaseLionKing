f22a3578387b2f5f3125e265452f1b94a2319dfc
Monira Mah <moma2202@student.miun.se>
2023-03-10 01:01:39 +0100

    - Download NPGSQL from NuGet - created manage user and filled in json - created the DAL (Data Access Layer) folder - created the class DbRepository in the DAL folder, where in we create connections to different things           - created readonly
    - created the GetAnimalByName method in our class
    - created an object to the DbRepository class in mainwindow
    - called on the GetAnimalByName method
         - made it async, Task and to return <Animal>
         - code to connect to database
    - created constructor where it can access connection                                                        string
    
    - created models map
    - created Animal class in models map
           - created property character_id & Character_name

--- ProgrammeringMotDatabaser/MainWindow.xaml.cs
+++ ProgrammeringMotDatabaser/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProgrammeringMotDatabaser.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,14 @@ namespace ProgrammeringMotDatabaser
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnsearch_Click(object sender, RoutedEventArgs e)
+        {
+            DbRepository db = new();
+
+            db.GetAnimalByName();
+
         }
     }
 }
