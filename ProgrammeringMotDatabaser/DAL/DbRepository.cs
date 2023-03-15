2c90058ab395cf9e842ac6ef7c6fdac1fc10fbe1
Monira Mah <moma2202@student.miun.se>
2023-03-15 11:42:49 +0100

    kommentar

--- ProgrammeringMotDatabaser/DAL/DbRepository.cs
+++ ProgrammeringMotDatabaser/DAL/DbRepository.cs
@@ -79,6 +79,7 @@ namespace ProgrammeringMotDatabaser.DAL
             string sqlQ = "SELECT * FROM animalspecie ORDER BY animalspeciename ASC";
             
             await using var dataSource = NpgsqlDataSource.Create(_connectionString);
+            // En kommentar från Moniras dator
             await using var command = dataSource.CreateCommand(sqlQ);
             await using var reader = await command.ExecuteReaderAsync();
             Animalspecie animalspecie = new Animalspecie();
